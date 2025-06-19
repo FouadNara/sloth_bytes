94537e4a0504248aa70b37a0b169a19dc828ccff
Fouad Nara <59171483+FouadNara@users.noreply.github.com>
2025-06-19 13:14:21 +0300

    Update birthday_candles.cpp
    function done

--- birthday_candles.cpp
+++ birthday_candles.cpp
@@ -1,1 +1,17 @@
+//this function iterates over the candles vector and saves the tallest candle it has seen with a counter
+//whenever we see a taller candle, we reset the counter to 1 and update the tallest candle
+int birthday_candles(vector<int>& candles){
+  int counter = 0;
+  int tallest = -1;
 
+  for(auto c : candles){
+    if(c > tallest){
+      counter = 1;
+      tallest = c;
+    }
+    else if(c == tallest){
+      counter++;
+    }
+  }
+  return counter;
+}
